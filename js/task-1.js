8c0c893b5362df4b878a9dbfca1a05d53dffbbec
sergeihodarkovski <sergeihodarkovski@gmail.com>
2024-03-21 17:05:24 +0100

    mod4

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+function isEnoughCapacity(products, containerSize) {
+  let totalSalary = 0;
+  const resSalarys = Object.values(products);
+  for (let index = 0; index < resSalarys.length; index++) {
+    totalSalary += resSalarys[index];
+  }
+  if (totalSalary <= containerSize) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
